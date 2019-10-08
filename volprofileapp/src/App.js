82248460a7f33873d26b6406ce108e873c7dc041
anthonypiazza <piazza.anthony.n@gmail.com>
2019-10-08 09:54:00 -0500

    Added Form and Result minimal styling Rd. 1

--- volprofileapp/src/App.js
+++ volprofileapp/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Link to="/">Home</Link>
+      <Link style={{ marginBottom: '150px', borderRadius: '10px', padding: '10px', border: '1px solid white', textDecoration: 'none', color: 'white' }} to="/">Home</Link>
       <Route exact path="/" render={(props) => <Form {...props} />} />
       <Route path="/results" render={(props) => <Results {...props} />} />
       </header>
